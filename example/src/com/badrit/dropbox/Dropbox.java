f29cbf3aac43958b7046c8f4968e2b65e2d2867c
Hazem Hagrass <hazem.hagrass@badrit.com>
2013-07-04 22:24:41 +0200

    Refactor and update license

--- example/src/com/badrit/dropbox/Dropbox.java
+++ example/src/com/badrit/dropbox/Dropbox.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, Twist and Shout, Inc. http://www.twist.com/
+ * Copyright (c) 2013, Hazem Hagrass
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
  */
 
 /**
- * @author yvonne@twist.com (Yvonne Yip)
+ * @author hazem.hagrass@badrit.com (Hazem Hagrass)
  */
 
 package com.badrit.dropbox;
@@ -198,62 +198,12 @@ public class Dropbox extends CordovaPlugin {
 		res.setKeepCallback(true);
 		callback.sendPluginResult(res);
 	}
-	// private void doDropboxTest() {
-	// try {
-	// final String TEST_DATA = "Hello Dropbox";
-	// final String TEST_FILE_NAME = "hello_dropbox.txt";
-	// DbxPath testPath = new DbxPath(DbxPath.ROOT, TEST_FILE_NAME);
-	//
-	// // Create DbxFileSystem for synchronized file access.
-	// DbxFileSystem dbxFs =
-	// DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
-	//
-	// // Print the contents of the root folder. This will block until we can
-	// // sync metadata the first time.
-	// List<DbxFileInfo> infos = dbxFs.listFolder(DbxPath.ROOT);
-	// mTestOutput.setText("\nContents of app folder:\n");
-	// for (DbxFileInfo info : infos) {
-	// mTestOutput.append("    " + info.path + ", " + info.modifiedTime + '\n');
-	// }
-	//
-	// // Create a test file only if it doesn't already exist.
-	// if (!dbxFs.exists(testPath)) {
-	// DbxFile testFile = dbxFs.create(testPath);
-	// try {
-	// testFile.writeString(TEST_DATA);
-	// } finally {
-	// testFile.close();
-	// }
-	// mTestOutput.append("\nCreated new file '" + testPath + "'.\n");
-	// }
-	//
-	// // Read and print the contents of test file. Since we're not making
-	// // any attempt to wait for the latest version, this may print an
-	// // older cached version. Use getSyncStatus() and/or a listener to
-	// // check for a new version.
-	// if (dbxFs.isFile(testPath)) {
-	// String resultData;
-	// DbxFile testFile = dbxFs.open(testPath);
-	// try {
-	// resultData = testFile.readString();
-	// } finally {
-	// testFile.close();
-	// }
-	// mTestOutput.append("\nRead file '" + testPath + "' and got data:\n    " +
-	// resultData);
-	// } else if (dbxFs.isFolder(testPath)) {
-	// mTestOutput.append("'" + testPath.toString() + "' is a folder.\n");
-	// }
-	// } catch (IOException e) {
-	// mTestOutput.setText("Dropbox test failed: " + e);
-	// }
-	// }
+	
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == REQUEST_LINK_TO_DBX) {
 			if (resultCode == Activity.RESULT_OK) {
-				// doDropboxTest();
 			} else {
 				Log.d("Dropbox", "Link to Dropbox failed or was cancelled.");
 			}
